45123c3abafb6d28b7c874ac86204f16796fe3b6
master <sungwoony@kakao.com>
2026-03-21 03:51:05 +0900

    링크 기능 추가

---
+++ link/202603.js
@@ -0,0 +1,58 @@
+const linkData = [
+    {
+        "page": 1,
+        "url": "https://timf.org/kr/sub/timf/notice.asp?mode=view&bid=1&s_type=&s_keyword=&s_cate=&idx=3523&page=1",
+        "icon": false,
+        "name": "표지-통영국제음악제"
+    },
+    {
+        "page": 3,
+        "url": "https://www.gugak.go.kr/site/main/index001",
+        "icon": false,
+        "name": "이습회-국립국악원"
+    },
+    {
+        "page": 4,
+        "url": "https://www.pccf.or.kr/board/list.do;jsessionid=C85E4448B8CCA14E586F1321D543362E?bbsId=sub01_01",
+        "icon": false,
+        "name": "가곡과 아리아의 밤- 평택아트센터"
+    },
+    {
+        "page": 5,
+        "url": "https://www.sejongpac.or.kr/portal/main/main.do",
+        "icon": false,
+        "name": "빅마더- 세종문화회관"
+    },
+    {
+        "page": 6,
+        "url": "https://www.ntok.go.kr/ntok/main.do",
+        "icon": false,
+        "name": "보허자- 국립극장"
+    },
+    {
+        "page": 7,
+        "url": "https://www.kbssymphony.org/ko/m/main/main.php",
+        "icon": false,
+        "name": "말러- KBS교향악단"
+    },
+    {
+        "page": 8,
+        "url": "https://www.gsartscenter.com/",
+        "icon": false,
+        "name": "웨인 맥그리거- GS아트센터"
+    },    
+        {
+        "page": 9,
+        "url": "https://www.gugak.go.kr/site/main/index001",
+        "icon": false,
+        "name": "토요명품-국립국악원"
+    },    
+        {
+        "page": 10,
+        "url": "https://new.igbf.kr/",
+        "icon": false,
+        "name": "CHANGE UP- 국악방송"
+    },    
+]
+
+export default linkData;
